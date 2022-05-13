97a9e44af7d3846c395617f424c9ab816409a390
ryuheimat <ryuheimat3@gmail.com>
2022-05-14 02:27:15 +0900

    add config

---
+++ config/mainnet.ts
@@ -0,0 +1,13 @@
+export const config = {
+  NETWORK_TYPE: "MAINNET",
+
+  CHAIN_ID: "secret-4",
+  CHAIN_NAME: "Secret 4",
+
+  SECRET_POST_ADDRESS: "https://bridge-api-manager.azure-api.net/",
+  SECRET_RPC: "https://bridge-api-manager.azure-api.net/",
+  // SECRET_WS: 'wss://bootstrap.secrettestnet.io:26667/websocket',
+  SECRET_LCD: "https://bridge-api-manager.azure-api.net/",
+
+  SCRT_EXPLORER_URL: "https://secretnodes.com/secret/chains/secret-4"
+}
---
+++ config/testnet.ts
@@ -0,0 +1,13 @@
+export const config = {
+  NETWORK_TYPE: "TESTNET",
+
+  CHAIN_ID: "pulsar-2",
+  CHAIN_NAME: "Pulsar 2",
+
+  SECRET_POST_ADDRESS: "http://testnet.securesecrets.org:1317",
+  SECRET_RPC: "http://testnet.securesecrets.org:26657/",
+  // SECRET_WS='wss://chainofsecrets.secrettestnet.io:26667/websocket',
+  SECRET_LCD: "http://testnet.securesecrets.org:1317/",
+
+  SCRT_EXPLORER_URL: "https://secretnodes.com/secret/chains/pulsar-2"
+}
